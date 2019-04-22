819f6d1ca771fa1c8242f8dc20116b5f7c7de72f
zonghaoyuan <1427412891@qq.com>
2019-04-22 21:59:23 +0800

    后台页面初步搭建，完成头部及部分侧边栏

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,61 @@
 <template>
-    <div>
-        后台管理页面
-    </div>
+  <el-container>
+    <el-header>
+      <div id="logo-box">
+        <img src="../assets/img/heima.png">
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu background-color="#333744" text-color="#fff" active-text-color="#ffd04b">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style lang="less" scoped>
-
+.el-container {
+  height: 100%;
+  .el-header {
+    display: flex;
+    justify-content: space-between;
+    padding: 0;
+    padding-right: 20px;
+    align-items: center;
+    background-color: #373d41;
+    #logo-box {
+      color: #fff;
+      font-size: 22px;
+      display: flex;
+      align-items: center;
+      img {
+        width: 50px;
+        height: 50px;
+        margin-right: 10px;
+      }
+    }
+  }
+  .el-aside {
+    background-color: #333744;
+  }
+  .el-main {
+    background-color: #eaedf1;
+  }
+}
 </style>
