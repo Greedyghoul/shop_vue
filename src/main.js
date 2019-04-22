88c7123bfe46c126257fa2ebb5275dd8610a08b4
zonghaoyuan <1427412891@qq.com>
2019-04-22 15:56:46 +0800

    安装并加载element-ui，并配置按需引入

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import router from './router'
 // 引入全局css样式
 import './assets/css/global.css'
 
+// 引入element-ui
+import ElementUI from 'element-ui'
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
