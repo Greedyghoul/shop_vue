4fde01f596e2a973fe654f6884de8386b3a0180c
zonghaoyuan <1427412891@qq.com>
2019-04-23 15:58:09 +0800

    后台主页左侧导航结构初步完成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,15 +8,31 @@
       <el-button type="info">退出</el-button>
     </el-header>
     <el-container>
-      <el-aside width="200px">
-        <el-menu background-color="#333744" text-color="#fff" active-text-color="#ffd04b">
-          <el-submenu index="1">
+      <el-aside :style="{width:isshow?'65px':'200px'}">
+        <div class="toggle_bar" :style="{width:isshow?'65px':'200px'}" @click="isshow=!isshow">|||</div>
+        <el-menu
+        background-color="#333744"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+        :unique-opened="true"
+        :collapse="isshow"
+        :collapse-transition="false"
+        >
+          <el-submenu index="1" :style="{width:isshow?'65px':'200px'}" >
             <template slot="title">
               <i class="el-icon-location"></i>
               <span>导航一</span>
             </template>
             <el-menu-item index="1-1">选项1</el-menu-item>
             <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-submenu>
+          <el-submenu index="2" :style="{width:isshow?'65px':'200px'}" >
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航二</span>
+            </template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
           </el-submenu>
         </el-menu>
       </el-aside>
@@ -26,7 +42,13 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      isshow: false
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
@@ -44,6 +66,7 @@ export default {}
       font-size: 22px;
       display: flex;
       align-items: center;
+      user-select: none;      // 禁止选中文字内容
       img {
         width: 50px;
         height: 50px;
@@ -53,6 +76,17 @@ export default {}
   }
   .el-aside {
     background-color: #333744;
+    .toggle_bar {
+      height: 25px;
+      background-color: #4a5064;
+      text-align: center;
+      color: #fff;
+      font-size: 12px;
+      line-height: 25px;
+      letter-spacing: 0.1em;   // 设置字与字之间的间隔
+      cursor: pointer;
+      user-select: none;
+    }
   }
   .el-main {
     background-color: #eaedf1;
