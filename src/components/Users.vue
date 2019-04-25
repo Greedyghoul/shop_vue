36244e22d500c61cc40eda2a98ac2e1cdd513c7c
zonghaoyuan <1427412891@qq.com>
2019-04-25 15:47:03 +0800

    完成用户管理分页功能

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -24,17 +24,30 @@
       <!-- 表格区 -->
       <el-table :data="tableData" stripe style="width: 100%" border>
         <el-table-column type="index" label="序号" width="60"></el-table-column>
-        <el-table-column prop="username" label="用户名" width="80"></el-table-column>
-        <el-table-column prop="mobile" label="手机号码" width="100"></el-table-column>
-        <el-table-column prop="role_name" label="角色" width="100"></el-table-column>
-        <el-table-column prop="email" label="邮箱" width="150"></el-table-column>
-        <el-table-column prop="mg_state" label="状态" width="80">
+        <el-table-column prop="username" label="用户名" width="100"></el-table-column>
+        <el-table-column prop="mobile" label="手机号码" width="120"></el-table-column>
+        <el-table-column prop="role_name" label="角色" width="120"></el-table-column>
+        <el-table-column prop="email" label="邮箱" width="160"></el-table-column>
+        <el-table-column prop="mg_state" label="状态" width="100">
           <!-- 此处为难点重点记忆 -->
           <el-switch v-model="info.row.mg_state" slot-scope="info"></el-switch>
           <!-- <span slot-scope="info">{{info.row}}</span> -->
         </el-table-column>
         <el-table-column prop="address" label="操作"></el-table-column>
       </el-table>
+
+      <!-- 分页区 -->
+      <div class="block">
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="queryData.pagenum"
+          :page-sizes="[2, 5, 10, 20]"
+          :page-size="2"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="4"
+        ></el-pagination>
+      </div>
     </el-card>
   </div>
 </template>
@@ -56,15 +69,28 @@ export default {
     }
   },
   methods: {
+    // 获取用户列表信息
     async getTableData() {
       const { data: dt } = await this.$http.get('/users', {
         params: this.queryData
       })
-      console.log(dt)
+      // console.log(dt)
       if (dt.meta.status !== 200) {
         return this.$message.error(dt.meta.msg)
       }
       this.tableData = dt.data.users
+    },
+    // 切换分页显示数据条数触发的回调函数
+    handleSizeChange(arg) {
+      // console.log(arg)
+      this.queryData.pagesize = arg
+      this.getTableData()
+    },
+    // 当前页码变化触发的回调函数
+    handleCurrentChange(arg) {
+      // console.log(arg)
+      this.queryData.pagenum = arg
+      this.getTableData()
     }
   }
 }
