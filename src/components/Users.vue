e821e9f71da2b203aef4a0359ee12972fb8a3edf
zonghaoyuan <1427412891@qq.com>
2019-04-25 21:21:59 +0800

    绘制对话框并构造添加用户表单，定义表单规则，并发送表单数据实现新用户添加

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -25,7 +25,40 @@
           </el-input>
         </el-col>
         <el-col :span="8">
-          <el-button type="primary">添加用户</el-button>
+          <!-- 添加新用户 -->
+          <el-button type="primary" @click="showAddDialog = true">添加用户</el-button>
+          <!-- 对话框 -->
+          <el-dialog
+            title="添加用户"
+            :visible.sync="showAddDialog"
+            width="50%"
+            center
+            @close="resetForm()"
+          >
+            <el-form
+              :model="addRuleForm"
+              :rules="addFormRules"
+              ref="addFormRef"
+              label-width="100px"
+            >
+              <el-form-item label="用户名" prop="username">
+                <el-input v-model="addRuleForm.username"></el-input>
+              </el-form-item>
+              <el-form-item label="密码" prop="password">
+                <el-input v-model="addRuleForm.password"></el-input>
+              </el-form-item>
+              <el-form-item label="邮箱" prop="email">
+                <el-input v-model="addRuleForm.email"></el-input>
+              </el-form-item>
+              <el-form-item label="手机号" prop="mobile">
+                <el-input v-model="addRuleForm.mobile"></el-input>
+              </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+              <el-button @click="showAddDialog = false">取 消</el-button>
+              <el-button type="primary" @click="addUser()">确 定</el-button>
+            </span>
+          </el-dialog>
         </el-col>
       </el-row>
 
@@ -42,10 +75,17 @@
           <!-- <span slot-scope="info">{{info.row}}</span> -->
         </el-table-column>
         <el-table-column prop="address" label="操作">
+          <!-- 添加操作按钮 -->
           <el-row>
             <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
             <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
-            <el-tooltip class="item" effect="dark" content="分配角色" placement="top" :enterable="false">
+            <el-tooltip
+              class="item"
+              effect="dark"
+              content="分配角色"
+              placement="top"
+              :enterable="false"
+            >
               <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
             </el-tooltip>
           </el-row>
@@ -61,7 +101,7 @@
           :page-sizes="[2, 5, 10, 20]"
           :page-size="2"
           layout="total, sizes, prev, pager, next, jumper"
-          :total="4"
+          :total="tot"
         ></el-pagination>
       </div>
     </el-card>
@@ -74,13 +114,43 @@ export default {
     this.getTableData()
   },
   data() {
+    var checkMobile = (rule, value, callback) => {
+      // 利用正则表达式制定手机号码规则
+      if (!/^1[35789]\d{9}$/.test(value)) {
+        // 格式错误，返回错误格式
+        return callback(new Error('手机号码格式不正确'))
+      }
+      // 正确，继续执行
+      callback()
+    }
     return {
+      tot: 0,
       tableData: [],
       // 查询用户数据需要提供的请求信息
       queryData: {
         query: '', // 查询参数
         pagenum: 1, // 当前页码
         pagesize: 2 // 每页显示数据条数
+      },
+      // 添加用户对话框显示开关
+      showAddDialog: false,
+      // 添加用户对话框表单规则
+      addRuleForm: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: ''
+      },
+      // 表单验证规则
+      addFormRules: {
+        username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+        password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
+        email: [{ required: true, message: '请输入邮箱地址', trigger: 'blur' }],
+        // 自定义检测规则应用
+        mobile: [
+          { required: true, message: '请输入手机号码', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ]
       }
     }
   },
@@ -95,6 +165,7 @@ export default {
         return this.$message.error(dt.meta.msg)
       }
       this.tableData = dt.data.users
+      this.tot = dt.data.total
     },
     // 切换分页显示数据条数触发的回调函数
     handleSizeChange(arg) {
@@ -111,6 +182,28 @@ export default {
     // 关键字搜索
     search() {
       this.getTableData()
+    },
+    // 关闭对话框时清空表单,故此处关闭事件需要加在对话框上，而不是form上
+    resetForm() {
+      this.$refs.addFormRef.resetFields()
+    },
+    // 添加新用户
+    addUser() {
+      this.$refs.addFormRef.validate(async valid => {
+        // console.log(valid)
+        // console.log(this.addRuleForm)
+        if (valid) {
+          const { data: dt } = await this.$http.post('users', this.addRuleForm)
+          // console.log(dt)
+          if (dt.meta.status !== 201) {
+            return this.$message.error(dt.meta.msg)
+          }
+          // 添加成功后，关闭对话框，弹窗提示添加成功，重新获取用户列表
+          this.showAddDialog = false
+          this.$message.success(dt.meta.msg)
+          this.getTableData()
+        }
+      })
     }
   }
 }
